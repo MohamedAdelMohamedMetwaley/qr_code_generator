efe932b84acc53a3ac9d8a5a50f48b2e61b42391
Mohamed Adel <mohamedadelzmd@gmail.com>
2024-08-26 12:44:03 +0300

    image downloads after being created

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const generateForm = document.getElementById("qr_form");
-const qr_image = document.getElementById("qr_image");
+const qrImage = document.getElementById("qr_image");
 
 generateForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const url = document.getElementById("qr_url").value;
   const size = "size=150x150";
   const qrlink = `https://api.qrserver.com/v1/create-qr-code/?${size}&data=`;
-  qr_image.src = qrlink + url;
+  const newImgSrc = qrlink + url;
+  qrImage.src = newImgSrc;
+  downloadImage(newImgSrc);
   url.value = "";
 });
+
+async function downloadImage(imageSrc) {
+  const image = await fetch(imageSrc);
+  const imageBlog = await image.blob();
+  const imageURL = URL.createObjectURL(imageBlog);
+
+  const link = document.createElement("a");
+  link.href = imageURL;
+  link.download = "qrcode";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
